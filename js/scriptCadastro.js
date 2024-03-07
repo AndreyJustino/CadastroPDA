1b2c49ba0c50567e62d8989446b47ce33c29e99a
AndreyJustino <andreyjustino2020@gmail.com>
2024-03-07 16:43:30 -0300

    cadastro

--- js/scriptCadastro.js
+++ js/scriptCadastro.js
@@ -1,5 +1,35 @@
+const buttonCadastrar = document.getElementById("cadastrar")
 const buttonLista = document.getElementById("verLista")
 
 buttonLista.addEventListener("click", () => {
     window.location.replace("../html/listaCadastro.html")
+})
+class Funcionario {
+    constructor (id,nome, cargo, contato){
+        this.id = id
+        this.nome = nome
+        this.cargo = cargo
+        this.contato = contato
+    }
+}
+
+let lista = []
+
+let id = 0
+
+function cadastrar (){
+    let nome = document.getElementById("name").value;
+    let cargo = document.getElementById("occupation").value;
+    let email = document.getElementById("contact").value;
+    
+    lista.push(new Funcionario(id,nome,cargo,email))
+
+    id++
+
+    localStorage.setItem("listaFuncionario", lista)
+    
+}
+
+buttonCadastrar.addEventListener("click", () => {
+    cadastrar()
 })
